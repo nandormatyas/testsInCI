e87775477765b67c550d2d097b05ad0f073e6f04
nandormatyas <nandor.matyas@gmail.com>
2018-03-04 14:19:53 +0100

    testing jenkins-tests

---
+++ src/app/hellotest.spec.ts
@@ -0,0 +1,31 @@
+
+
+describe('hellotest', () => {
+
+    let expected = '';
+    let notExpected = '';
+    let expectMatch = null;
+
+    beforeEach(()=>{
+        expected = 'hellotest';
+        notExpected = 'hellotest123';
+    expectMatch = new RegExp(/^hello/);
+    });
+
+    afterEach(()=>{
+        expected = '';
+        notExpected = '';
+    });
+
+    it('checks if hellotest is hellotest',
+     () => expect('hellotest').toBe(expected)
+    );     ;
+    it('checks if hellotest is not hellotest',
+     () => expect('hellotest').not.toBe(notExpected)
+    );
+    it('checks if hellotest starts with',
+    () => expect('hellotest').toMatch(expectMatch)
+   );
+
+
+});
