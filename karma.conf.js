6efa39c8b94f0ee77bcf05109cb6eaf1db9ad31c
nandormatyas <nandor.matyas@gmail.com>
2018-03-04 16:38:16 +0100

    disable phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['Chrome', 'Firefox'],
     singleRun: true,
 
     junitReporter : {
